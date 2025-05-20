c0e7e810269d79a77c0273315655d32f8d0a770a
COOKIEHUB <nailaaurel24@gmail.com>
2025-05-20 16:59:50 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('fs');
+require('dotenv').config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.commands = new Collection();
+
+require('./handlers/eventHandler')(client);
+require('./handlers/commandHandler')(client);
+
+client.login(process.env.TOKEN);
